6384a4bb0cc44f55ad190a9c57e9ccee8e2b171c
martin-1415 <martin.fischer1415@gmail.com>
2025-01-09 16:28:10 +0100

    cicd the first step

--- src/app/components/Countdown.tsx
+++ src/app/components/Countdown.tsx
@@ -4,7 +4,7 @@ import React, {useEffect, useState} from 'react';
 export default function Countdown() {
 
     const calculateTimeLeft = () => {
-        const difference: number = new Date('2025-08-09 09:00') - new Date();
+        const difference: number =  new Date('2025-08-09 09:00').getTime() - new Date().getTime();
         if (difference > 0) {
             return {
                 days: Math.floor(difference / (1000 * 60 * 60 * 24)),
@@ -29,7 +29,7 @@ export default function Countdown() {
 
     return (
         <div className="flex justify-center">
-                {timeLeft.days}d {timeLeft.hours}h {timeLeft.minutes}m {timeLeft.seconds}s
+                {timeLeft!.days}d {timeLeft!.hours}h {timeLeft!.minutes}m {timeLeft!.seconds}s
         </div>
 
     );
