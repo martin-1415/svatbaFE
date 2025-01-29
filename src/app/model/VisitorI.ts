477c9fe8cff3f5aec1b5e97404aad7de5a384cc1
martin-1415 <martin.fischer1415@gmail.com>
2025-01-30 00:24:17 +0100

    connection to the database, saving new visitor

---
+++ src/app/model/VisitorI.ts
@@ -0,0 +1,5 @@
+export interface VisitorI {
+    _id: {$oid:string};
+    name: string;
+    welcomeMessage: string;
+}
--- src/app/newVisitor/page.tsx
+++ src/app/newVisitor/page.tsx
@@ -1,46 +1,76 @@
 "use client";
 
 import { FormEvent } from 'react'
+import { useEffect, useState } from "react";
+import {VisitorI} from "../model/VisitorI";
 
 export default function Page() {
+    const [listOfVisitors, setData] = useState< VisitorI[] | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(process.env.NEXT_PUBLIC_API_URL!.concat('/getAll'));
+                const list:VisitorI[] = await response.json();
+                console.log(list);
+                setData(list);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     async function onSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
 
         const apiUrl:string = process.env.NEXT_PUBLIC_API_URL!;
         const formData = new FormData(event.currentTarget)
+        let response: Response;
+        let hash: string = " ";
 
-        // try {
-            console.log(apiUrl.concat("/newVisitor"));
-            const response: Response = await fetch(apiUrl.concat("/newVisitor"), {
-                method: 'POST',
-                body: formData,
-            })
+        try {
+            response = await fetch(apiUrl.concat("/newVisitor"), {
+                            method: 'POST',
+                            body: formData,
+                            })
             if (!response.ok) {
                 throw new Error(`Response status: ${response.status}`);
             }
-            // const json = await response.json();
-            // console.log(json);
-        // } catch{
-        //     console.error("Saving visitor failed:" + response.toString());
-        // }
+            // Await the response parsing
+            hash = await response.text();
 
-        // Handle response if necessary
-        //const data = await response.json()
+        } catch{
+            console.error("Saving visitor failed");
+            return;
+        }
 
+        alert(process.env.NEXT_PUBLIC_URL!.concat('?id=').concat(hash));
     }
 
     return (
         <form  className="p-6" onSubmit={onSubmit}>
             <div className="m-2">
                 <input name="name"/>
-                <label htmlFor="name"> Jmeno</label>
+                <label htmlFor="name"> Jméno</label>
             </div>
             <div className="m-2">
                 <input name="welcomeMessage"/>
-                <label htmlFor="welcomeMessage ">Uvitaci zprava</label>
+                <label htmlFor="welcomeMessage ">Uvítací zpráva (...pozvali na)</label>
             </div>
-            <button type="submit" className="bg-blue-500 text-white rounded-md px-2 py-1">Submit</button>
+            <button type="submit" className="bg-blue-500 text-white rounded-md px-2 py-1">Odeslat</button>
+
+            {listOfVisitors && listOfVisitors.length > 0 ? (
+                listOfVisitors.map((visitor) => (
+                    <ul key={visitor._id.$oid}>
+                        <li><a className="text-blue-600" href={process.env.NEXT_PUBLIC_URL!.concat('?id=').concat(visitor._id.$oid)}>{process.env.NEXT_PUBLIC_URL!.concat('?id=').concat(visitor._id.$oid)}</a>  {visitor.name}     {visitor.welcomeMessage}   </li>
+                    </ul>
+                ))
+            ) : (
+                <p>Loading...</p>
+            )}
+
          </form>
     )
 }
